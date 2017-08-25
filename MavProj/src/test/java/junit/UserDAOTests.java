1940668e8bafa70d262edd4b32fa67688457bc97
Haisam <elkewidyhaisam@gmail.com>
2017-08-25 10:27:14 -0400

    DAOImpl class changes commit

---
+++ MavProj/src/test/java/junit/UserDAOTests.java
@@ -0,0 +1,38 @@
+package junit;
+
+import org.junit.Test;
+
+public class UserDAOTests {
+	
+	@Test
+	public void createUserTest(){
+		
+		
+	}
+	
+	@Test
+	public void updateUserTest(){
+		
+		
+	}
+	
+	@Test
+	public void deleteUserTest(){
+		
+		
+	}
+	
+	@Test
+	public void getFullNameTest(){
+		
+		
+	
+		
+		
+	}
+	
+	
+	
+	
+
+}
