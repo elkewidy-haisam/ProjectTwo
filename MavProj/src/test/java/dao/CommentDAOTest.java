3611d177a461018cfaee3d7c256a369d260cc63f
Daniel <felix0654@gmail.com>
2017-08-27 12:22:07 -0400

    change6

--- MavProj/src/test/java/dao/CommentDAOTest.java
+++ MavProj/src/test/java/dao/CommentDAOTest.java
@@ -1,5 +1,12 @@
 package dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+
 //import java.util.logging.Logger;
 
 import org.junit.Test;
@@ -66,19 +73,88 @@ public class CommentDAOTest {
 		
 	}
 	
+	//done
+	//actual class tested is not completed
 	@Test
+	@Ignore
 	public void testGetUserComment(){
-		
+		//create a new dao
+		CommentDAOImpl myCommentDAO = new CommentDAOImpl();
+		//create a new bean
+		UserComment expected = new UserComment(1,1,"Example");
+		//retrieve the result from the database and compare to your
+		//expected data
+		UserComment actual = myCommentDAO.getUserComment(1, 1);
+		//run the assertion statements for the comparison
+		assertEquals(expected.getUsersID(), actual.getUsersID());
+		assertEquals(expected.getCommentID(), actual.getCommentID());
 	}
 	
+	//done
 	@Test
 	public void testGetAllUserComments(){
+		//create a new dao
+		CommentDAOImpl myCommDao = new CommentDAOImpl();
+		//very big shortcut
+		//List<UserComment> myUserCommentList = new ArrayList<UserComment>();
+		List<UserComment> myUserCommentList = myCommDao.getAllUserComments();
+	}
+	
+	//done
+	@Test
+	public void testAddComment(){
+		//new dao
+		CommentDAOImpl myCommentDAOImpl = new CommentDAOImpl();
+		//create a new bean to insert
+		Comment myComment = new Comment();
+		myComment.setComment_id(4);
+		myComment.setUser_id(4);
+		//call the add function
+		myCommentDAOImpl.addComment(myComment);
 		
 	}
 	
+	//done
 	@Test
-	public void testAddComment(){
+	public void testDeleteComment(){
+		//create dao
+		CommentDAOImpl myCommentDAOImpl = new CommentDAOImpl();
+		//create bean
+		Comment myComment = new Comment();
+		//add the bean
+		myCommentDAOImpl.addComment(myComment);
+		//delete the bean
+		myCommentDAOImpl.deleteComment(myComment);
+	}
+	
+	//done
+	@Test
+	public void testGetComment(){
+		//create dao
+		CommentDAOImpl myCommentDAOImpl = new CommentDAOImpl();
+		//create an expected bean
+		Comment expected = new Comment();
+		expected.setComment_id(1);
+		expected.setUser_id(1);
+		//find the actual
+		Comment actual = myCommentDAOImpl.getComment(1);
+		//run the assertion tests
+		assertEquals(expected.getComment_id(), actual.getComment_id());
+		assertEquals(expected.getUser_id(), actual.getUser_id());
 		
+	}
+	
+	//done
+	@Test
+	public void testUpdateComment(){
+		//create a dao
+		CommentDAOImpl myCommentDAOImpl = new CommentDAOImpl();
+		//get a bean from the database
+		Comment myComment = myCommentDAOImpl.getComment(1);
+		//modify the bean
+		myComment.setComment_id(10);
+		//add the bean back to the database
+		myCommentDAOImpl.updateComment(myComment);
 	}
 	
 }
